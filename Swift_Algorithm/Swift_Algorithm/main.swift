144bcca314b29f859388f3822186f60903dd6346
Yang Kyeongmo <gaeng1107@gmail.com>
2021-09-20 22:43:56 +0900

    file move

--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -4,13 +4,11 @@
 //
 //  Created by Gaeng on 2021/02/02.
 //
-let arr = readLine()!.split(separator: " ").map { Int($0)! }
-let a = arr[0], b = arr[1], v = arr[2]
-let d = (v - b) / (a - b)
-
-if (v - b) % (a - b) != 0 {
-    print(d + 1)
+var pointList: [(Int, Int)] = []
+(1...Int(readLine()!)!).forEach{ _ in
+    let input = readLine()!.split{$0==" "}.map{Int(String($0))!}
+    pointList.append((input[1], input[0]))
 }
-else {
-    print(d)
+for o in pointList.sorted(by: {$0 < $1}) {
+    print(o.1, o.0)
 }
